9a333cd88456fc73e7d6357ee35430d46792959c
Nolan Davidson <nolan.davidson@gmail.com>
2014-02-23 21:12:28 -0500

    Added Windows recipe

---
+++ recipes/windows.rb
@@ -0,0 +1,25 @@
+if node.platform_family?("windows")
+  arch = node['kernel']['machine']
+  #puts "Arch is #{arch}"
+  path = ""
+  display_name = ""
+
+  if arch.include?("_64")
+    path = "x86_64"
+    display_name = "Boundary Meter (x64 edition)"
+  elsif arch.include?("i386")
+    path = "i686"
+    display_name = "Boundary Meter (x86 edition)"
+  end
+
+
+  windows_package "#{display_name}" do
+    source "https://windows.boundary.com/#{path}/bprobe-current.msi"
+    options "/l*v bprobe.log INSTALLTOKEN=2jUPQEHUZD6X9AMjCdGyaWQNUEP:8a8MHZXrCxLhKaxhPOKJSS42BA2"
+    action :install
+  end
+
+  service "bprobe" do
+    action [ :start, :enable ]
+  end
+end
