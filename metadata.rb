956b870a7704b8733d93ee63696fe2754db88f1d
Nick Silkey <nick.silkey@rackspace.com>
2014-04-11 15:03:11 -0500

    version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "ops@boundary.com"
 license          "Apache 2.0"
 description      "Installs/Configures bprobe"
 long_description "Installs/Configures bprobe"
-version          "0.2"
+version          "0.3"
 
 %w{ ubuntu debian rhel centos amazon scientific }.each do |os|
   supports os
